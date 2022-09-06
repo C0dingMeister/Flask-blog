e5f687247e13958198050d6c9949327ae3590b9c
akshat <akshat.sharma1337@gmail.com>
2022-09-06 12:29:15 +0530

    added env variables

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
+from dotenv import load_dotenv
 
-
+load_dotenv()
+print()
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=10)
-app.config['SECRET_KEY'] = "randomwords"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 jwt = JWTManager(app)
 CORS(app)
