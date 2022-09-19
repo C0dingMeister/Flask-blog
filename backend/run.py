24c1791b8804e53c824fbbd34d3122fceae5da71
akshat <akshat.sharma1337@gmail.com>
2022-09-19 11:22:25 +0530

    New libraries added

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,9 @@
 from app import app
 
 if __name__ == '__main__':
-    # with open('default.png','rb') as file:
-    #     print(file.read())
+    
+    #git remote add origin https://github.com/C0dingMeister/Flask-blog.git
+    # git branch -M main
+    # git push -u origin main
     
     app.run(debug=True)
