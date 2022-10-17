af7094289562a4cb3bd93f998c55eb172346f5a3
akshat <akshat.sharma1337@gmail.com>
2022-10-17 15:09:27 +0530

    Defining a particular flash message

---
+++ frontend/src/components/FlashMessage.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Alert, Collapse } from "react-bootstrap";
+import { FlashContext } from "./context/FlashProvider";
+
+export default function FlashMessage() {
+    const { flashMessage, visible, hideFlash } = useContext(FlashContext);
+    return(
+        <Collapse in={visible}>
+            <div>
+                <Alert variant={flashMessage.type || 'info'} dismissible onClose={hideFlash}>
+                    {flashMessage.message}
+                </Alert>
+            </div>
+        </Collapse>
+    );
+}
